aadb882de52902de5580fe663b918e21889eb04e
YomingXyz <rivafuadistudent21@gmail.com>
2026-06-13 23:31:08 +0700

    Create Pemanggil.py

---
+++ Pemanggil.py
@@ -0,0 +1,9 @@
+import Rivafuadi012 as mt
+
+m1 = [[1, 2], [3, 4]]
+m2 = [[5, 6], [7, 8]]
+
+print(mt.Pertambahanmatrix(m1,m2))
+print(mt.Penguranganmatrix(m1,m2))
+print(mt.Perkalianmatrix(m1,m2))
+print(mt.Pembagianmatrix(m1,m2))
